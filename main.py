1fa9dcdb9bf2f93d50d16a9ddacbc4c26ff82b0b
Alexandr Chernega <chernegaam@gmail.com>
2022-04-08 11:24:36 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from aiogram import Bot, Dispatcher, executor, types
+import logging
+import aiohttp
+import os
+import urllib.parse
+
+BOT_TOKEN = os.environ['TOKEN']
+api_base = os.environ['API_BASE']
+api_pics = os.environ['API_PICS']
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def help_pls(message: types.Message):
+    await message.answer('''Данный бот предназначен для поиска мемов по описанию. Чтобы найти мем, просто отправьте 
+    боту его описание в текстовом виде, и он выдаст вам первые 10 мемов подходящих под ваще описание. Команда /next 
+    - выдача следующих 10 мемов, команда /info - информация о создателях бота, API и тп''')
+
+
+@dp.message_handler(commands=['info'])
+async def info_pls(message: types.Message):
+    await message.answer('''Lol данная команда пока не готова''')
+
+
+@dp.message_handler(commands=['next'])
+async def next_meme(message: types.Message):
+    await message.answer('''Lol данная команда пока не готова''')
+
+
+@dp.message_handler()
+async def get_meme(message: types.Message):
+    text = message.text
+    session = aiohttp.ClientSession()
+    response = await session.get(f"{api_base}?s={urllib.parse.quote(text.encode('utf-8'))}")
+    memes = await response.json()
+    res_pics = []
+    for meme in memes:
+        uuid = meme['id']
+        res = await session.get(f"{api_pics}/{uuid[:1]}/{uuid[2:3]}/{uuid[4:]}")
+        res_pics.append(res)
+    for i in range(10):
+        await bot.send_photo(message.chat.id, types.InputFile.from_url(res_pics[i]))
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
