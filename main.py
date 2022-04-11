6a74924380ecd7b223944688351184b1744e2454
Alexandr Chernega <chernegaam@gmail.com>
2022-04-11 19:11:11 +0300

    Added sending all pictures as a photo collage

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.utils.exceptions import *
 import logging
 import aiohttp
 import os
 import urllib.parse
 
-BOT_TOKEN = os.environ['TOKEN']
+BOT_TOKEN = '5267899048:AAHf1qikSVTAhilni6oCXePjIhv8hnGT_kg'
 api_base = os.environ['API_BASE']
 api_pics = os.environ['API_PICS']
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
+
+
+@dp.errors_handler(exception=BotBlocked)
+async def error_bot_blocked(update: types.Update, exception: BotBlocked):
+    print(f"Меня заблокировал пользователь!\nСообщение: {update}\nОшибка: {exception}")
+    return True
 
 
 @dp.message_handler(commands=['start', 'help'])
@@ -36,9 +43,10 @@ async def get_meme(message: types.Message):
     response = await session.get(f"{api_base}/images?limit=10&q={urllib.parse.quote(text)}")
     memes = await response.json()
     await session.close()
+    pics = types.MediaGroup()
     for meme in memes:
-        link = meme['link']
-        await bot.send_photo(message.chat.id, types.InputFile.from_url(link))
+        pics.attach_photo(types.InputFile.from_url(meme['link']))
+    await bot.send_media_group(message.chat.id, media=pics)
 
 
 if __name__ == '__main__':
